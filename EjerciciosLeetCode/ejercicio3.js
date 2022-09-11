67718300a2578d8b8cff6d52308f2b8d58d96e55
Alonso Morante <morantealo@hotmail.com>
2022-09-10 20:07:23 -0500

    Finalización de ejercicios leetcode

--- EjerciciosLeetCode/ejercicio3.js
+++ EjerciciosLeetCode/ejercicio3.js
@@ -1,13 +1,12 @@
-function adultos(arr) {
-    const resultado = arr.filter(i => {
-        if (i < 18 || i > 70) {
-            return false
-        } else {
-            return true
+function twoSum(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let n = i + 1; n < nums.length; n++) {
+            if (nums[i] + nums[n] == target) {
+                return [i, n]
+            }
         }
-    })
-
-    return resultado
+    }
 }
 
-console.log(adultos([28, 45, 17, 21, 17, 70]))
+
+console.log(twoSum([2,7,11,15], 9))
